f1b16e53fec78f595f14959337226de23d396c92
Harsh0R <harshradadiya9999@gmail.com>
2024-07-19 01:53:57 +0530

    update

--- rest_api1/dist/index.js
+++ rest_api1/dist/index.js
@@ -12,13 +12,17 @@ const PORT = process.env.PORT;
 console.log("PORT ", PORT);
 const app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: false }));
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
+    console.log("Called Middal ware");
+    next();
+});
+app.get("/", (req, res) => {
     return res.send(`Home Page`);
 });
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
     return res.send(data_json_1.default);
 });
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
     const html = `
     <ul>
     ${data_json_1.default.map((user) => `<li>${user.name} ${user.last_name}</li> `).join(" ")}
@@ -26,20 +30,57 @@ app.get('/users', (req, res) => {
     `;
     res.send(html);
 });
-app.route('/api/users/:id').get((req, res) => {
+app
+    .route("/api/users/:id")
+    .get((req, res) => {
     const id = Number(req.params.id);
     const user = data_json_1.default.find((user) => user.id == id);
     return res.json(user);
-}).patch((req, res) => {
-    return res.json({ status: "pandding..." });
-}).delete((req, res) => {
-    return res.json({ status: "pandding..." });
-});
-app.post('/api/users', (req, res) => {
+})
+    .patch((req, res) => {
     const body = req.body;
+    const id = Number(req.params.id);
+    const userIndex = data_json_1.default.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+        return res
+            .status(404)
+            .json({ status: "error", message: "User not found" });
+    }
+    const updateUser = Object.assign(Object.assign({}, data_json_1.default[userIndex]), req.body);
+    data_json_1.default[userIndex] = updateUser;
+    fs_1.default.writeFile("./src/data.json", JSON.stringify(data_json_1.default), (err) => {
+        if (err) {
+            return res
+                .status(500)
+                .json({ status: "error", message: "Failed to write to file" });
+        }
+        return res.json({ status: "success", user: updateUser });
+    });
+})
+    .delete((req, res) => {
+    const id = Number(req.params.id);
+    const userIndex = data_json_1.default.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+        return res
+            .status(404)
+            .json({ status: "error", message: "User not found" });
+    }
+    data_json_1.default.splice(userIndex, 1);
+    fs_1.default.writeFile("./src/data.json", JSON.stringify(data_json_1.default), (err) => {
+        if (err) {
+            return res
+                .status(500)
+                .json({ status: "error", message: "Failed to write to file" });
+        }
+        return res.json({ status: "success", message: "User deleted" });
+    });
+});
+app.post("/api/users", (req, res) => {
+    const body = req.body;
+    console.log("Body ==> ", body);
     data_json_1.default.push(Object.assign(Object.assign({}, body), { id: data_json_1.default.length + 1 }));
-    fs_1.default.writeFile('./data.json', JSON.stringify(data_json_1.default), (_err) => {
-        return res.json({ status: "pandding..." });
+    fs_1.default.writeFile("./src/data.json", JSON.stringify(data_json_1.default), (_err) => {
+        return res.json({ status: "Success", id: data_json_1.default.length });
     });
 });
 app.listen(PORT, () => {
